fc56eae75b727894961715f4d88e0591b1dc70ea
shim_cc <36291577+shim-cc@users.noreply.github.com>
2018-02-14 16:22:35 +0900

    ボタンの名前変更

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "button";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -47,9 +47,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 12);
+            this.label1.Size = new System.Drawing.Size(81, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Click to button1";
+            this.label1.Text = "Click to button";
             // 
             // Form1
             // 
